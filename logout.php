aff5217aad70546451a4c7c506a23a5101c28fcf
Denis <denis05skenderii@gmail.com>
2026-05-21 14:55:34 +0200

    fix

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include '/database/db.php';
+    include './database/db.php';
     $_SESSION = array();
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '/database/db.php';
+include './database/db.php';
 
 $fail = "";
 $success = "";
